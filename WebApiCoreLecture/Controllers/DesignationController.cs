030753a3a0007cf2a6e57d92a5c6b8ab678f4fe0
Mohd Mustkeem <mohdmustkeem78624@gmail.com>
2022-07-13 16:21:24 +0530

    Add project files.

---
+++ WebApiCoreLecture/Controllers/DesignationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCoreLecture.Model;
+
+namespace WebApiCoreLecture.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesignationController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+        public DesignationController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        // GET: api/lDesignation
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblDesignation>>> GetTblDesignation()
+        {
+            return await _context.tblDesignations.ToListAsync();
+        }
+    }
+}
---
+++ WebApiCoreLecture/Migrations/20220707124817_Init.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiCoreLecture.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblDesignations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Designation = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblDesignations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblEmployees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 150, nullable: true),
+                    LastName = table.Column<string>(maxLength: 150, nullable: true),
+                    Email = table.Column<string>(maxLength: 250, nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Doj = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<string>(maxLength: 20, nullable: true),
+                    IsMarried = table.Column<int>(nullable: false),
+                    IsActive = table.Column<int>(nullable: false),
+                    DesignationID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblEmployees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblDesignations");
+
+            migrationBuilder.DropTable(
+                name: "tblEmployees");
+        }
+    }
+}
